540bf478366e7a8197e4a28be00c61ff01f3cebc
zengming <zengming@juwang.cn>
2018-10-10 18:22:33 +0800

    update

--- example/sysinfo/main.go
+++ example/sysinfo/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/260by/tools/sys/mem"
 	"github.com/260by/tools/sys/load"
 	"github.com/260by/tools/sys/net"
+	psdisk "github.com/shirou/gopsutil/disk"
 )
 
 func main()  {
@@ -21,4 +22,11 @@ func main()  {
 	fmt.Printf("Load1: %v Load5: %v Load15: %v\n", loadAvg[0], loadAvg[1], loadAvg[2])
 	tcpState := net.TCPState()
 	fmt.Printf("TCP State: %v\n", tcpState)
+
+	ioStats, err := psdisk.IOCounters("/dev/sdb1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("IO: %v\n", ioStats)
 }
