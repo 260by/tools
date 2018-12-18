8546639d995c9a3aa1fdff96f2ce1dfca8eb1e8c
zengming <zengming@juwang.cn>
2018-12-18 17:14:56 +0800

    修改请求错误后执行下一个循环

--- proxy/gethttp.go
+++ proxy/gethttp.go
@@ -20,7 +20,7 @@ func main() {
 	}
 	proxy := strings.Split(string(f), "\n")
 
-	baseURL := "http://www.evenote.cn:9000"
+	baseURL := "http://www.vogued.cn"
 
 	for _, p := range proxy {
 		p = "http://" + p
@@ -37,7 +37,7 @@ func main() {
 				return c, nil
 			},
 			MaxIdleConnsPerHost:   10,                      //每个host最大空闲连接
-			ResponseHeaderTimeout: time.Millisecond * 2000, //数据收发超时
+			ResponseHeaderTimeout: time.Millisecond * 3000, //数据收发超时
 		}
 
 		client := &http.Client{
@@ -45,10 +45,20 @@ func main() {
 			Timeout:   time.Second * 5,
 		}
 
-		resp, err := client.Get(baseURL)
+		request, err := http.NewRequest("GET", baseURL, nil)
 		if err != nil {
 			fmt.Println(err)
+			return
+		}
+
+		request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36")
+
+		resp, err := client.Do(request)
+		if err != nil {
+			fmt.Println(err)
+			continue
 		}
 		fmt.Println(resp.StatusCode)
+		time.Sleep(2 * time.Second)
 	}
 }
