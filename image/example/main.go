5573f0ef13bcc432d6060483d5a0c3f4d1ffbb4f
zengming <zengming@juwang.cn>
2018-12-29 17:32:47 +0800

    修改示例

--- image/example/main.go
+++ image/example/main.go
@@ -7,24 +7,25 @@ import (
 
 func main()  {
 	// 根据指定宽度按比例缩放图片
-	err := image.Scale("./images/1.jpeg", "./images/1-100.jpeg", 100,)
+	err := image.Scale("./images/1.jpeg", "./images/1-100.jpeg", 200,)
 	if err != nil {
 		panic(err)
 	}
 
 	// 根据指定宽度和高度缩放图片
-	err = image.Thumbnail("./images/1.jpeg", "./images/1-100.jpeg", 100, 200)
+	err = image.Thumbnail("./images/1.jpeg", "./images/1-236x354.jpeg", 236, 354)
 	if err != nil {
 		panic(err)
 	}
 
 	// 通过x,y坐标剪切图片，原图为440x660
-	err = image.Cut("./images/1.jpeg", "./images/1-440x660.jpeg", 0, 0, 440, 610)
+	err = image.Cut("./images/1.jpeg", "./images/1-440x660.jpeg", 0, 0, 440, 640)
 	if err != nil {
 		panic(err)
 	}
 
-	w, h, err := image.GetImgWidthHeight("http://a-cdn.vogued.cn/images/20181130/78673733b0504f0e86b1ccc8118299af.jpeg")
+	// 获取图片宽度和高度
+	w, h, err := image.GetImgWidthHeight("./images/1.jpeg")
 	if err != nil {
 		panic(err)
 	}
